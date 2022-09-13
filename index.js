5f3a23afaed25746577e2687b5f922ccdcd7a9d7
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-09-13 13:37:13 +0100

    add eslint file

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener('click', function showMenu(){
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('show-menu');
-})
-
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('show-menu');
+});
 
 const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(navLink => {
-    navLink.addEventListener('click',function closeMenu(){
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('show-menu');     
-    })
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('show-menu');
+  });
 });
