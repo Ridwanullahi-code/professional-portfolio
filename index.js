ecfd70a612bf8f4a88aec756b20c051cf0ffe26e
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-09-15 22:01:02 +0100

    correct linter error

--- index.js
+++ index.js
@@ -15,39 +15,39 @@ navLinks.forEach((navLink) => {
 });
 
 const projects = [{
-  'name':'Tonic',
-  'description':'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  'image':'images/portoflio-card/Snapshoot-Portfolio1.png',
-  'technologies':['html','css','javascript','github'],
-  'link':'https://ridwanullahi-code.github.io/professional-portfolio/',
-  'demo':'https://github.com/Ridwanullahi-code/professional-portfolio'
-  },
-  {
-    'name':'Tonic',
-    'description':'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'image':'images/portoflio-card/Snapshoot-Portfolio1.png',
-    'technologies':['html','css','javascript','github'],
-    'link':'https://ridwanullahi-code.github.io/professional-portfolio/',
-    'demo':'https://github.com/Ridwanullahi-code/professional-portfolio'
-  },
-  {
-    'name':'Multi-Post<br>Stories',
-    'description':'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'image':'images/portoflio-card/Snapshoot-Portfolio1.png',
-    'technologies':['html','css','javascript','github'],
-    'link':'https://ridwanullahi-code.github.io/professional-portfolio/',
-    'demo':'https://github.com/Ridwanullahi-code/professional-portfolio'
-  },
+  name: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  image: 'images/portoflio-card/Snapshoot-Portfolio1.png',
+  technologies: ['html', 'css', 'javascript', 'github'],
+  link: 'https://ridwanullahi-code.github.io/professional-portfolio/',
+  demo: 'https://github.com/Ridwanullahi-code/professional-portfolio',
+},
+{
+  name: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  image: 'images/portoflio-card/Snapshoot-Portfolio1.png',
+  technologies: ['html', 'css', 'javascript', 'github'],
+  link: 'https://ridwanullahi-code.github.io/professional-portfolio/',
+  demo: 'https://github.com/Ridwanullahi-code/professional-portfolio',
+},
+{
+  name: 'Multi-Post<br>Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  image: 'images/portoflio-card/Snapshoot-Portfolio1.png',
+  technologies: ['html', 'css', 'javascript', 'github'],
+  link: 'https://ridwanullahi-code.github.io/professional-portfolio/',
+  demo: 'https://github.com/Ridwanullahi-code/professional-portfolio',
+},
 
-  {
-  'name':'Multi-Post<br>Stories',
-    'description':'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'image':'images/portoflio-card/Snapshoot-Portfolio1.png',
-    'technologies':['html','css','javascript','github'],
-    'link':'https://ridwanullahi-code.github.io/professional-portfolio/',
-    'demo':'https://github.com/Ridwanullahi-code/professional-portfolio'
-  }
-]; 
+{
+  name: 'Multi-Post<br>Stories',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  image: 'images/portoflio-card/Snapshoot-Portfolio1.png',
+  technologies: ['html', 'css', 'javascript', 'github'],
+  link: 'https://ridwanullahi-code.github.io/professional-portfolio/',
+  demo: 'https://github.com/Ridwanullahi-code/professional-portfolio',
+},
+];
 
 // <=============== modal popup information ====================>
 const projectTitle = document.querySelectorAll('.project-title');
@@ -55,35 +55,35 @@ const projectImage = document.querySelectorAll('.project-image');
 const projectDescpt = document.querySelectorAll('.description');
 const techList = document.querySelectorAll('.tools');
 
-
 const cardTitle = document.querySelector('.card-title');
 const cardImage = document.querySelector('.card-image');
 const modalTech = document.querySelector('.tech');
 const description = document.querySelector('.card-description');
 const modalButton = document.querySelectorAll('.modal-button');
 
-
 const cards = {
-  'description':"MLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with therelea",
-  'technologies':['ruby','bootstrap']
- }
- 
+  description: "MLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a  galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with therelea",
+  technologies: ['ruby', 'bootstrap'],
+};
+
 // store each project's information into object
 for (let i = 0; i < projects.length; i += 1) {
-  projectTitle[i].innerHTML = projects[i]['name']
-  projectImage[i].src = projects[i]['image']
-  projectDescpt[i].innerHTML = projects[i]['description'] 
-  techList[0].children[i].innerHTML = projects[i]['technologies'][i]
-  techList[1].children[i].innerHTML = projects[i]['technologies'][i]
-  techList[2].children[i].innerHTML = projects[i]['technologies'][i]
-  techList[3].children[i].innerHTML = projects[i]['technologies'][i]
-  modalTech.children[i].innerHTML = projects[i]['technologies'][i]
-  modalTech.children[4].innerHTML = cards['technologies'][0]
-  modalTech.children[5].innerHTML = cards['technologies'][1]
+  projectTitle[i].innerHTML = projects[i].name;
+  projectImage[i].src = projects[i].image;
+  projectDescpt[i].innerHTML = projects[i].description;
+  techList[0].children[i].innerHTML = projects[i].technologies[i];
+  techList[1].children[i].innerHTML = projects[i].technologies[i];
+  techList[2].children[i].innerHTML = projects[i].technologies[i];
+  techList[3].children[i].innerHTML = projects[i].technologies[i];
+  modalTech.children[i].innerHTML = projects[i].technologies[i];
 }
 
+for (let i = 0; i < 2; i += 1) {
+  const list = document.createElement('li');
+  list.innerHTML = cards.technologies[i];
+  modalTech.appendChild(list);
+}
 
-console.log(modalTech)
 // get the modal popup initial elements
 const buttons = document.querySelectorAll('.see-project-btn');
 const modal = document.querySelector('.card');
@@ -101,7 +101,6 @@ for (let i = 0; i < buttons.length; i += 1) {
     cardImage.src = projects[i].image;
     modalButton[0].href = projects[i].demo;
     modalButton[1].href = projects[i].link;
-
   });
 }
 // <=============== Function to close modal ===============>
@@ -115,4 +114,3 @@ backdrop.addEventListener('click', () => {
   modal.classList.remove('active');
   backdrop.classList.remove('show');
 });
-
